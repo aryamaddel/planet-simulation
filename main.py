b00ec3cf90bfa645f8a3434bfc08566f995e4d2a
Arya Maddel <maddelarya@gmail.com>
2023-06-18 13:51:21 +0530

    formatting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+
 pygame.init()
 
 width, height = 800, 800
@@ -15,8 +16,8 @@ DARK_GREY = (80, 78, 81)
 class Planet:
     AU = 149.6e6 * 1000
     G = 6.67428e-11
-    SCALE = 250/AU  # 1AU = 100 pixels
-    TIMESTEP = 3600*24  # 1 day
+    SCALE = 250 / AU  # 1AU = 100 pixels
+    TIMESTEP = 3600 * 24  # 1 day
 
     def __init__(self, x, y, radius, colour, mass):
         self.x = x
@@ -38,13 +39,13 @@ class Planet:
     def attraction(self, other):
         other_x, other_y = other.x, other.y
         distance_x, distance_y = other_x - self.x, other_y - self.y
-        distance = math.sqrt((distance_x**2)+(distance_y**2))
+        distance = math.sqrt((distance_x**2) + (distance_y**2))
         if other.sun:
             self.distance_to_sun = distance
         force = self.G * self.mass * other.mass / distance**2
         theta = math.atan2(distance_y, distance_x)
-        force_x = math.cos(theta)*force
-        force_y = math.sin(theta)*force
+        force_x = math.cos(theta) * force
+        force_y = math.sin(theta) * force
         return force_x, force_y
 
     def update_position(self, planets):
@@ -66,7 +67,7 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
-    sun = Planet(0, 0, 30, YELLOW,  1.98892 * 10**30)
+    sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
     sun.sun = True
 
     earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
